0728a3ba800cb4cd47a77130b22f304dbd34bda8
LuisDantas <luishenriquedantas@usp.br>
2023-06-08 21:53:24 -0300

    Eduardo salva nois

---
+++ manipulaBEstrela/bEstrela.c
@@ -0,0 +1,74 @@
+#include "bEstrela.h"
+#include "../arquivos/arquivos.h"
+#include "cabecalhoBEstrela.h"
+#include "../cabecalho/cabecalho.h"
+#include "../manipulaRegistros/registro.h"
+#include "../manipulaIndices/inteiro/indiceInteiro.h"
+
+bool ArvoreCriar(char nomeArquivo[], char arquivoDados[]){
+    FILE *arvore, *dados;
+    if(!AbreArquivo(&arvore, nomeArquivo, "wb", NULL)) return false;
+
+    CABECALHO_B *cabecalhoArvore = CabecalhoBCriar();
+    CabecalhoBEscreve(arvore, cabecalhoArvore);
+
+    if(!AbreArquivo(&dados, arquivoDados, "rb", NULL)) return false;
+    CABECALHO *cabecalhoDados = CabecalhoCriar();
+    LeCabecalhoDoArqBinario(cabecalhoDados, dados);
+
+    //Percorre arquivo binario lendo os registros
+    long int offsetProximoRegistro = 17; //será o offset do proximo registro durante o loop
+    long int offsetInicioRegistro = 17;
+    DADOS *registroAuxiliar = RegistroCriar();
+
+    //A flag verifica se o id foi lido e serve para saber se chegou no final do arquivo
+    int flag = LerRegBinario(dados, registroAuxiliar, &offsetProximoRegistro);
+
+    //pos é utilizada para armazenar a posicao que o campo deve ser inserido
+    int pos = 0;
+    int i;
+    //le registros até o final do arquivo, inserindo no vetor de indices
+    for(i = 0; flag != 0; i++){
+        if(GetRegistroRemovido(registroAuxiliar) == '0'){
+
+
+
+
+            //INSERCAO NA ARVORE!!!
+
+
+
+
+            pos++;
+        }
+
+        DesalocaCamposVariaveis(registroAuxiliar);
+
+        offsetInicioRegistro = offsetProximoRegistro;
+
+        flag = LerRegBinario(dados, registroAuxiliar, &offsetProximoRegistro); 
+    }
+
+    //se nao existem registros no arquivo
+    if(i==0) ErroArquivo();
+}
+
+bool ArvoreInserir(FILE *arvore, DADOS *registro, CABECALHO_B *cabecalho){
+
+}
+
+ValoresRetorno Insert(int CURRENT_RRN, DADOS_INT KEY, DADOS_INT PROMO_KEY, int PROMO_R_CHILD){
+    //se nó não existe, promove a chave
+    if(CURRENT_RRN == NIL){
+        PROMO_KEY = KEY;
+        PROMO_R_CHILD = NIL;
+
+        return PROMOTION;
+    }else{
+        //leia página CURRENT_RRN e armazene em PAGE
+        //procure por KEY em PAGE
+        //faça POS igual a posição em que KEY ocorre ou deveria ocorrer
+    }
+
+
+}
--- manipulaBEstrela/bEstrela.h
+++ manipulaBEstrela/bEstrela.h
@@ -3,9 +3,13 @@
     #include <stdio.h>
     #include <stdlib.h>
     #include <string.h>
+    #include <stdbool.h>
 
     #define MAXCHAVES 4
     #define TAM_PAGE 76
+    #define NIL -1
+
+    typedef enum {PROMOTION, NO_PROMOTION, ERROR} ValoresRetorno;
 
     struct chave {
         int C;
